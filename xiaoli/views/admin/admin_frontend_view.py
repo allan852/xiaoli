2c8ce71ec396200badaff564691f0cb3d6356e5b
zhangp <zhangp522@gmail.com>
2015-10-21 15:34:54 +0800

    add baidu editor

--- xiaoli/views/admin/admin_frontend_view.py
+++ xiaoli/views/admin/admin_frontend_view.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from flask import Blueprint, render_template, redirect, url_for, request
+import os
+import re
+import json
+
+from flask import Blueprint, render_template, redirect, url_for, request,make_response
 from flask.ext.babel import gettext as _
 from flask.ext.paginate import Pagination
-from xiaoli.models import Account, Plan
+from xiaoli.models import Account, Plan,Uploader
 from xiaoli.models.session import db_session_cm
+from xiaoli.config import setting
+
 
 __author__ = 'zouyingjun'
 
@@ -96,6 +102,109 @@ def plan_delete(plan_id):
     u"""删除方案"""
     return render_template("admin/plan/index.html")
 
+@admin_frontend.route('/plan/new')
+def plan_new():
+    u"""新建方案"""
+    return render_template("admin/plan/new.html")
+
+@admin_frontend.route('/upload/',methods=['GET', 'POST','OPTIONS'])
+def upload():
+    u"""UEditor文件上传接口
+    config 配置文件
+    result 返回结果
+    """
+    mimetype = 'application/json'
+    result = {}
+    action = request.args.get('action')
+    # 解析JSON格式的配置文件
+
+    CONFIG = setting.EDITOR_CONFIG
+    if action == 'config':
+        # 初始化时，返回配置文件给客户端
+        result = CONFIG
+    elif action in ('uploadimage', 'uploadfile', 'uploadvideo'):
+        # 图片、文件、视频上传
+        if action == 'uploadimage':
+            fieldName = CONFIG.get('imageFieldName')
+            config = {
+                "pathFormat": CONFIG['imagePathFormat'],
+                "maxSize": CONFIG['imageMaxSize'],
+                "allowFiles": CONFIG['imageAllowFiles']
+            }
+        elif action == 'uploadvideo':
+            fieldName = CONFIG.get('videoFieldName')
+            config = {
+                "pathFormat": CONFIG['videoPathFormat'],
+                "maxSize": CONFIG['videoMaxSize'],
+                "allowFiles": CONFIG['videoAllowFiles']
+            }
+        else:
+            fieldName = CONFIG.get('fileFieldName')
+            config = {
+                "pathFormat": CONFIG['filePathFormat'],
+                "maxSize": CONFIG['fileMaxSize'],
+                "allowFiles": CONFIG['fileAllowFiles']
+            }
+        if fieldName in request.files:
+            field = request.files[fieldName]
+            uploader = Uploader(field, config, 'public/static')
+            result = uploader.getFileInfo()
+        else:
+            result['state'] = '上传接口出错'
+    elif action in ('uploadscrawl'):
+        # 涂鸦上传
+        fieldName = CONFIG.get('scrawlFieldName')
+        config = {
+            "pathFormat": CONFIG.get('scrawlPathFormat'),
+            "maxSize": CONFIG.get('scrawlMaxSize'),
+            "allowFiles": CONFIG.get('scrawlAllowFiles'),
+            "oriName": "scrawl.png"
+        }
+        if fieldName in request.form:
+            field = request.form[fieldName]
+            uploader = Uploader(field, config, app.static_folder, 'base64')
+            result = uploader.getFileInfo()
+        else:
+            result['state'] = '上传接口出错'
+    elif action in ('catchimage'):
+        config = {
+            "pathFormat": CONFIG['catcherPathFormat'],
+            "maxSize": CONFIG['catcherMaxSize'],
+            "allowFiles": CONFIG['catcherAllowFiles'],
+            "oriName": "remote.png"
+        }
+        fieldName = CONFIG['catcherFieldName']
+        if fieldName in request.form:
+            source = []
+        elif '%s[]' % fieldName in request.form:
+            source = request.form.getlist('%s[]' % fieldName)
+        _list = []
+        for imgurl in source:
+            uploader = Uploader(imgurl, config, app.static_folder, 'remote')
+            info = uploader.getFileInfo()
+            _list.append({
+                'state': info['state'],
+                'url': info['url'],
+                'original': info['original'],
+                'source': imgurl,
+            })
+        result['state'] = 'SUCCESS' if len(_list) > 0 else 'ERROR'
+        result['list'] = _list
+    else:
+        result['state'] = '请求地址出错'
+    result = json.dumps(result)
+    if 'callback' in request.args:
+        callback = request.args.get('callback')
+        if re.match(r'^[\w_]+$', callback):
+            result = '%s(%s)' % (callback, result)
+            mimetype = 'application/javascript'
+        else:
+            result = json.dumps({'state': 'callback参数不合法'})
+    res = make_response(result)
+    res.mimetype = mimetype
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Access-Control-Allow-Headers'] = 'X-Requested-With,X_Requested_With'
+    return res
 
 @admin_frontend.route('/plan/publish/<int:plan_id>')
 def plan_publish(plan_id):
