34c6c67229de2b2d52f0e2a0bb8d5b4b26407eaf
allan852 <zou5945@gmail.com>
2015-10-21 13:35:32 +0800

    fix bug

--- xiaoli/utils/logs/colored_logging.py
+++ xiaoli/utils/logs/colored_logging.py
@@ -57,6 +57,7 @@ class Logging:
         ## create log handler.
         print "colored_logging handlers length = %s" % len(logger.handlers)
         if len(logger.handlers) <= 0:
+            print "colored_logging add handler"
             if log_file is not None:
                 handler = logging.FileHandler(log_file)
             else:
@@ -68,7 +69,6 @@ class Logging:
             
             logger.addHandler(handler)
             logger.setLevel(log_level)
-        
         return logger
 
 
--- xiaoli/utils/logs/logger.py
+++ xiaoli/utils/logs/logger.py
@@ -5,4 +5,4 @@ from xiaoli.utils.logs.colored_logging import Logging
 print "common_logger to %s" % setting.LOG_FILE
 print "api_logger to %s" % setting.API_LOG_FILE
 common_logger = Logging.getLogger('xiaoli', log_file=setting.LOG_FILE, log_level=setting.LOG_LEVEL)
-api_logger = Logging.getLogger('xiaoli', log_file=setting.API_LOG_FILE, log_level=setting.LOG_LEVEL)
+api_logger = Logging.getLogger('xiaoliapi', log_file=setting.API_LOG_FILE, log_level=setting.LOG_LEVEL)
