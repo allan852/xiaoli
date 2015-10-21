0cca0070fe48986a87351927df9c081f7d72917f
allan852 <zou5945@gmail.com>
2015-10-21 12:37:15 +0800

    fix bug

--- xiaoli/utils/logs/logger.py
+++ xiaoli/utils/logs/logger.py
@@ -2,5 +2,7 @@
 from xiaoli.config import setting
 from xiaoli.utils.logs.colored_logging import Logging
 
+print "common_logger to %s" % setting.LOG_FILE
+print "api_logger to %s" % setting.API_LOG_FILE
 common_logger = Logging.getLogger('xiaoli', log_file=setting.LOG_FILE, log_level=setting.LOG_LEVEL)
 api_logger = Logging.getLogger('xiaoli', log_file=setting.API_LOG_FILE, log_level=setting.LOG_LEVEL)
