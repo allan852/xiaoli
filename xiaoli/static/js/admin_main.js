6ee1956bc13e6baebe5f854888bea6cc46183ca2
allan852 <zou5945@gmail.com>
2015-10-28 17:18:22 +0800

    所有列表页删除增加确认。

--- xiaoli/static/js/admin_main.js
+++ xiaoli/static/js/admin_main.js
@@ -3,8 +3,25 @@
  */
 
 $(function(){
+    // 处理 select2
     var $keywordSelectElement = $(".admin-plan-form #plan-keywords");
     if ($keywordSelectElement.length > 0){
         $keywordSelectElement.select2();
+    }
+
+    //处理action-prompt，需要确认的操作
+    var $actionPromptElement = $(".action-prompt");
+    if ($actionPromptElement.length > 0){
+        $actionPromptElement.on("click", function(e){
+            e.preventDefault();
+            var $element = $(e.currentTarget),
+                promptText = $element.data('prompt'),
+                href = $element.attr('href');
+            if (!!promptText && !!href){
+                if(window.confirm(promptText)){
+                    window.location.href = href;
+                }
+            }
+        })
     }
 });
