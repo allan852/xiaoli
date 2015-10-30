e1c8fcbe7b3abe0ea3a1b2ef6df5fd60305c2b19
allan852 <zou5945@gmail.com>
2015-10-30 17:35:15 +0800

    fix bug in plan in api interface with join

--- xiaoli/views/api_v1_view.py
+++ xiaoli/views/api_v1_view.py
@@ -396,12 +396,13 @@ def plans():
         key_word_id = request.args.get("key_word_id", None)
         res = api_response()
         with db_session_cm() as session:
-            plans = session.query(Plan).outerjoin(Plan.content).outerjoin(Plan.keywords).\
+            plans = session.query(Plan).\
                 filter(Plan.status == Plan.STATUS_PUBLISH)
             if search_key:
                 plans = plans.filter(Plan.title.like('%' + search_key + '%'))
             if key_word_id:
                 plans = plans.filter(Plan.id == key_word_id)
+            api_logger.info(plans)
             paginate = Page(total_entries=plans.count(), entries_per_page=per_page, current_page=page)
             results = plans.offset(paginate.skipped()).limit(paginate.entries_per_page()).all()
 
