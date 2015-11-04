b09d3271887b66b25bb68c57e9230fe308955162
allan852 <zou5945@gmail.com>
2015-11-04 18:43:16 +0800

    fix 002 migrations

--- migrations/versions/002_add_average_score_cloumn_to_accounts.py
+++ migrations/versions/002_add_average_score_cloumn_to_accounts.py
@@ -6,13 +6,13 @@ def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
     Account.metadata.bind = migrate_engine
-    score_average = Column('average_score', Integer(2), default=0)
+    score_average = Column('average_score', Integer, default=0)
     create_column(score_average, Account.__table__)
 
-    score_count = Column('score_count', Integer(11), default=0)
+    score_count = Column('score_count', Integer, default=0)
     create_column(score_count, Account.__table__)
 
-    score_total = Column('score_count', Integer(11), default=0)
+    score_total = Column('score_total', Integer, default=0)
     create_column(score_total, Account.__table__)
 
 
